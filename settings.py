cdf638cb53be40cb0b845260dc63b0b3697af663
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-04-23 14:27:01 +0300

    update common settings

--- settings.py
+++ settings.py
@@ -1,1 +1,10 @@
 MODEL_FILE = 'data/model.h5'
+BOX_HEIGHT = 68     # 75th percentile 190.25, mean average 153.22972972972974
+BOX_WIDTH = 157     # 75th percentile 118.0, mean average 110.66216216216216
+
+CSV_RESULTS = 'data/raw_results.csv'
+ORIGINAL_IMAGES = 'data/original_data'
+
+#TEST_NUM = 300
+#TRAIN_NUM = 200
+#VALIDATION_NUM = 300
