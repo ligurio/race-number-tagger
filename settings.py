6d3510d5e4f42994d57fb367b64f093cd456c14f
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-05-04 14:28:59 +0300

    replace fixed numbers for data subsets by proportions

--- settings.py
+++ settings.py
@@ -1,10 +1,7 @@
 MODEL_FILE = 'data/model.h5'
-BOX_HEIGHT = 68     # 75th percentile 190.25, mean average 153.22972972972974
-BOX_WIDTH = 157     # 75th percentile 118.0, mean average 110.66216216216216
-
 CSV_RESULTS = 'data/raw_results.csv'
 ORIGINAL_IMAGES = 'data/original_data'
 
-#TEST_NUM = 300
-#TRAIN_NUM = 200
-#VALIDATION_NUM = 300
+IMAGE_SUBSETS = { 'train': 50, 'validate': 35, 'test': 15 }  # proportion of images in subsets (percents)
+BOX_HEIGHT = 157    # 75th percentile 190.25, mean average 153.22972972972974
+BOX_WIDTH = 68      # 75th percentile 118.0, mean average 110.66216216216216
