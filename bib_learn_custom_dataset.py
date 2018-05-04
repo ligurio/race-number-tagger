ee5593b6381419f8c5f9e6ca5e01b70e0979243a
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-05-04 18:28:31 +0300

    first working version of script to learn on my own dataset

--- bib_learn_custom_dataset.py
+++ bib_learn_custom_dataset.py
@@ -1,35 +1,36 @@
 # https://github.com/tanmayb123/MNIST-CNN-in-Keras/blob/master/customtrain.py
+# MNIST model example https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.models import model_from_json
+from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense
+from bib_prepare_dataset import create_dir
+from bib_prepare_dataset import find_files
+import h5py
+import settings
+import os
 
-# FIXME
-img_width, img_height = 28, 28
+base_dir = './data/race_numbers/'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
 
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
-train_samples = 60000
-validation_samples = 10000
-epoch = 30
+train_samples = len([ f for f in find_files(train_dir, '*.jpg') ])
+validation_samples = len([ f for f in find_files(validation_dir, '*.jpg') ])
+test_samples = len([ f for f in find_files(test_dir, '*.jpg') ])
+epochs = 30
+batch_size = 16
 
-"""
-ModelCheckpoint https://keras.io/callbacks/
-
-data structure https://gist.github.com/fchollet/0830affa1f7f19fd47b06d4cf89ed44d
-
-"""
-
+# model = load_model(settings.MODEL_FILE)
 
 # ** Model Begins **
 model = Sequential()
-model.add(Convolution2D(16, 5, 5, activation='relu', input_shape=(img_width, img_height, 3)))
+model.add(Conv2D(16, (5, 5), activation='relu', input_shape=(settings.BOX_WIDTH, settings.BOX_HEIGHT, 3)))
 model.add(MaxPooling2D(2, 2))
-
-model.add(Convolution2D(32, 5, 5, activation='relu'))
+model.add(Conv2D(32, (5, 5), activation='relu'))
 model.add(MaxPooling2D(2, 2))
-
 model.add(Flatten())
 model.add(Dense(1000, activation='relu'))
 
@@ -43,28 +44,36 @@ model.compile(loss='binary_crossentropy',
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+        zoom_range=0.2)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2)
 
 train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
+        train_dir,
+        target_size=(settings.BOX_WIDTH, settings.BOX_HEIGHT),
         batch_size=32,
         class_mode='categorical')
 
-validation_generator = test_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
+validation_generator = validation_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(settings.BOX_WIDTH, settings.BOX_HEIGHT),
         batch_size=32,
         class_mode='categorical')
 
 model.fit_generator(
         train_generator,
         samples_per_epoch=train_samples,
-        nb_epoch=epoch,
+        epochs=epochs,
         validation_data=validation_generator,
-        nb_val_samples=validation_samples)
+        validation_steps=validation_samples)
 
-model.save_weights('mnistneuralnet.h5')
+score = model.evaluate_generator(validation_generator, validation_samples/batch_size, workers=12)
+print("Total: ", len(validation_generator.filenames))
+print("Loss: ", score[0], "Accuracy: ", score[1])
+print model.summary()
+
+model.save(settings.MODEL_FILE, overwrite=True)
+model.save_weights(settings.MODEL_WEIGHTS_FILE, overwrite=True)
