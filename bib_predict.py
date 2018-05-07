031c98f4f79699c308f314ed123c97d4ff07acd3
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-05-07 12:39:00 +0300

    more or less working version of prediction script

--- bib_predict.py
+++ bib_predict.py
@@ -1,38 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import numpy
-import cv2
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D
-from keras.layers import Flatten, Dense
+from keras.models import load_model
+from keras.preprocessing import image
+from bib_prepare_dataset import image_split
+from bib_prepare_dataset import img_size
+import matplotlib.pyplot as plt
 import sys
+import settings
+import numpy as np
 
-img_width, img_height = 28, 28
+def predict_image(img):
+    arr = np.array(img).reshape((settings.BOX_WIDTH, settings.BOX_HEIGHT, 3))
+    arr = np.expand_dims(arr, axis=0)
+    prediction = model.predict(arr)[0]
+    bestclass = ''
+    bestconf = -1
+    for n in [0,1,2,3,4,5,6,7,8,9]:
+            if (prediction[n] > bestconf):
+                    bestclass = str(n)
+                    bestconf = prediction[n]
 
-def create_model():
-    model = Sequential()
-    model.add(Convolution2D(16, 5, 5, activation='relu', input_shape=(img_width, img_height, 3)))
-    model.add(MaxPooling2D(2, 2))
-    model.add(Convolution2D(32, 5, 5, activation='relu'))
-    model.add(MaxPooling2D(2, 2))
-    model.add(Flatten())
-    model.add(Dense(1000, activation='relu'))
-    model.add(Dense(10, activation='softmax'))
-    model.summary()
-    return model
+    return bestclass, bestconf
 
-img = cv2.imread(sys.argv[1])
-img = cv2.resize(img, (img_width, img_height))
-model = create_model()
-model.load_weights('./model-train.h5')
-arr = numpy.array(img).reshape((img_width,img_height,3))
-arr = numpy.expand_dims(arr, axis=0)
-prediction = model.predict(arr)[0]
-bestclass = ''
-bestconf = -1
-for n in [0,1,2,3,4,5,6,7,8,9]:
-	if (prediction[n] > bestconf):
-		bestclass = str(n)
-		bestconf = prediction[n]
-print 'I think this digit is a ' + bestclass + ' with ' + str(bestconf * 100) + '% confidence.'
+img_path = sys.argv[1]
+print "Image:", img_path
+model = load_model(settings.MODEL_FILE)
+img_width, img_height = img_size(img_path)
+for piece in image_split(img_width, img_height, settings.BOX_WIDTH, settings.BOX_HEIGHT):
+    img = image.load_img(img_path, target_size=(img_width, img_height))
+    bestclass, bestconf = predict_image(image.img_to_array(img))
+    print 'I think this digit is a ' + bestclass + ' with ' + str(bestconf * 100) + '% confidence.'
