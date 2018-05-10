db4a8a3f09562a12bf6103dd9daa7847720deb4b
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-05-10 15:59:00 +0300

    add resize of original images in the mnist data

--- bib_learn_mnist.py
+++ bib_learn_mnist.py
@@ -1,3 +1,10 @@
+# https://hackernoon.com/how-tensorflows-tf-image-resize-stole-60-days-of-my-life-aba5eb093f35
+# https://stackoverflow.com/questions/45024328/resize-mnist-in-tensorflow
+# https://www.tensorflow.org/api_docs/python/tf/image/resize_images
+# https://github.com/yunjey/domain-transfer-network/blob/master/prepro.py
+
+# source: https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+
 from __future__ import print_function
 import keras
 from keras.datasets import mnist
@@ -6,19 +13,53 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
-import matplotlib.pyplot as plt
+from PIL import Image
 import numpy as np
 import settings
+import cv2
+
+def rotate(l):
+
+    rotated = []
+    for i in xrange(len(l), 0, -1):
+        rotated.append(l[i - 1])
+    return rotated
+
+
+def show(array):
+
+    cv2.namedWindow('Resized image', cv2.WINDOW_NORMAL)
+    cv2.imshow('Resized image', array)
+    print("Press any key")
+    cv2.waitKey()
+
+
+def resize_images(image_arrays, size=[32, 32]):
+
+    image_arrays = (image_arrays * 255).astype('uint8')
+    resized_image_arrays = np.zeros([image_arrays.shape[0]] + rotate(size))
+    for i, image_array in enumerate(image_arrays):
+        image = Image.fromarray(image_array)
+        resized_image = image.resize(size, resample=Image.BILINEAR)
+        resized_image_arrays[i] = np.asarray(resized_image)
+
+    return np.expand_dims(resized_image_arrays, 3)
+
 
 batch_size = 128
 num_classes = 10
 epochs = 15
-
 # input image dimensions
-img_rows, img_cols = 28, 28
+img_rows, img_cols = settings.BOX_HEIGHT, settings.BOX_WIDTH
+# image dimensions in dataset
+size = [settings.BOX_WIDTH, settings.BOX_HEIGHT]
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print("resize images (28 x 28) --> (%s)" % size)
+x_train = resize_images(x_train, size=size)
+x_test = resize_images(x_test, size=size)
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -29,34 +70,11 @@ else:
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
     input_shape = (img_rows, img_cols, 1)
 
-"""
-from skimage.transform import resize
-new_shape = (29, 29)
-X_train = np.empty(shape=(x_train.shape[0],) + new_shape)
-for idx in xrange(x_train.shape[0]):
-    X_train[idx] = resize(X_train[idx], new_shape)
-
-x_train = X_train
-"""
-
-"""
-# plot 4 images as gray scale
-plt.subplot(221)
-plt.imshow(x_train[0], cmap=plt.get_cmap('gray'))
-plt.subplot(222)
-plt.imshow(x_train[1], cmap=plt.get_cmap('gray'))
-plt.subplot(223)
-plt.imshow(x_train[2], cmap=plt.get_cmap('gray'))
-plt.subplot(224)
-plt.imshow(x_train[3], cmap=plt.get_cmap('gray'))
-# show the plot
-plt.show()
-"""
-
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
+
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
@@ -89,12 +107,11 @@ model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+print(model.summary())
 
-#model_json = model.to_json()
-#with open("model-train.json", "w") as json_file:
-#    json_file.write(model_json)
-#model.save_weights("model-train.h5")
-
+# save model
+model_json = model.to_json()
+with open("model-train.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights(settings.MODEL_WEIGHTS_FILE)
 model.save(settings.MODEL_FILE, overwrite=True)
-
-# model = load_model(settings.MODEL_FILE)
