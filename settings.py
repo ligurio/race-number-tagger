1a34757fa3fee8dfc3608f1aa04e46259afabb1c
Sergey Bronnikov <sergeyb@bronevichok.ru>
2018-05-10 17:55:50 +0300

    update settings
    - change data set proportion
    - add different names for mnist and real models file names

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 
-MODEL_FILE = 'data/model.h5'
-MODEL_WEIGHTS_FILE = 'data/model-weights.h5'
+MODEL_FILE_MNIST = 'data/model_mnist.h5'
+MODEL_FILE_REAL = 'data/model_real.h5'
+MODEL_WEIGHTS_FILE_MNIST = 'data/model-weights-mnist.h5'
+MODEL_WEIGHTS_FILE_REAL = 'data/model-weights-real.h5'
 CSV_RESULTS = 'data/raw_results.csv'
 ORIGINAL_IMAGES = 'data/original_data'
-IMAGE_SUBSETS = { 'train': 50, 'validate': 35, 'test': 15 }  # proportion of images in subsets (percents)
-BOX_HEIGHT = 157    # 75th percentile 190.25, mean average 153.22972972972974
-BOX_WIDTH = 68      # 75th percentile 118.0, mean average 110.66216216216216
+IMAGE_SUBSETS = { 'train': 85, 'validate': 10, 'test': 5 }  # proportion of images in subsets (percents)
+BOX_HEIGHT = 157
+BOX_WIDTH = 68
 PERCENTILE = 75
